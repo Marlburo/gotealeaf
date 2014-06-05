53270e766f85f4bd98c1cd8371ff164da5ec4ab0
Marlburo <lui_ca45@hotmail.com>
2014-06-05 17:51:42 -0400

    mod 5

--- lecture2.rb
+++ lecture2.rb
@@ -10,7 +10,10 @@ num1 = gets.chomp
 say "What's the second number"
 num2 = gets.chomp
 
-say "+) add -) subtract *) multiply /) divide"
+say "+ to add" 
+say "- to subtract" 
+say "* to multiply" 
+say "/ to divide"
 operator = gets.chomp
 
 if operator == '+'
