1262e9574943bbd1ae918c2d9961c0470064a6da
Marlburo <lui_ca45@hotmail.com>
2014-06-05 15:17:23 -0400

    fixed tabs

--- lecture2.rb
+++ lecture2.rb
@@ -1,10 +1,10 @@
+#upload to master branch
 #
 #
 #
-#tab size
 
 def say(msg)
-	puts "=> #{msg}"
+  puts "=> #{msg}"
 end
 
 say "What's the first number"
@@ -13,17 +13,17 @@ num1 = gets.chomp
 say "What's the second number"
 num2 = gets.chomp
 
-say "1) add 2) subtract 3) multiply 4) divide"
+say "+) add -) subtract *) multiply /) divide"
 operator = gets.chomp
 
-if operator == '1'
-	result = num1.to_i + num2.to_i
-elsif operator == '2'
-	result = num1.to_i - num2.to_i
-elsif operator == '3'
-	result = num1.to_i * num2.to_i
-elsif operator == '4'
-	result = num1.to_f / num2.to_f
+if operator == '+'
+  result = num1.to_i + num2.to_i
+elsif operator == '-'
+  result = num1.to_i - num2.to_i
+elsif operator == '*'
+  result = num1.to_i * num2.to_i
+elsif operator == '/'
+  result = num1.to_f / num2.to_f
 end
 	
 say "Result is #{result}"
--- variable_scope.rb
+++ variable_scope.rb
@@ -1,10 +1,11 @@
 #variable scope
-#tab size
+#upload to master branch
+#its not finished yet
 item1 = [1, 2, 'HELLO', 4]
+
 string = gets.chomp
 
-
 def weird_method(var)
-	item1 = var
+  item1 = var
 end
 
