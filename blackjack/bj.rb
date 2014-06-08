59a2dd50aebcc5c279de918e5000a1273af7cd96
Marlburo <lui_ca45@hotmail.com>
2014-06-07 20:20:51 -0400

    blckj

---
+++ blackjack/bj.rb
@@ -0,0 +1,23 @@
+#blackjack
+
+class blackjack	
+	
+	@@decks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J","Q","K"].product(["heart", "spade", "diamond", "club"])* number_of_decks
+	def shoe
+		@shoe = decks
+	end
+	
+	def bin
+	
+	end
+
+	#def deck(number_of_decks)
+		#@@decks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J","Q","K"].product(["heart", "spade", "diamond", "club"])* number_of_decks
+	#end
+
+	def player_name
+		@player_name = player_name
+	end
+
+end
+
