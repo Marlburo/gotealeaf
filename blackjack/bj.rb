3ac3f8ae9f9ccb0614398428a9bc109ebffc275e
Marlburo <lui_ca45@hotmail.com>
2014-06-09 16:53:24 -0400

    still working on this

--- blackjack/bj.rb
+++ blackjack/bj.rb
@@ -1,10 +1,13 @@
 #blackjack
-
+# Im going to use binding.pry, slice, shuffle, sample, unshift
+# going to use circular buffers for the deck, the shoe and the bin
 class Blackjack
 	
 	#@@number_of_decks = 1
 	#@@decks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J","Q","K"].product(["heart", "spade", "diamond", "club"])* number_of_decks
-	@@shoe = []
+	@@stack_shoe = []
+	@@stack_bin = []
+	@@deck_buff_size = @@shoe.lenght
 	
 	#this is where all the available cards are stored
 	def shoe
@@ -28,7 +31,7 @@ class Blackjack
 
 	#all the cards
 	def deck(number_of_decks)
-		@@shoe = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J","Q","K"].product(["heart", "spade", "diamond", "club"])* number_of_decks
+		@@stack_shoe = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J","Q","K"].product(["heart", "spade", "diamond", "club"])* number_of_decks
 	end
 
 	#names of the players
